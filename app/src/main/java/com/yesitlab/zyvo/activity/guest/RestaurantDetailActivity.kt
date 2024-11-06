95742e0a301d631f1c47530399793d7e5f783d9f
Nikunj Gupta <64137813+Nikunjgupta-dev@users.noreply.github.com>
2024-11-06 14:34:59 +0530

    nikunj commit -6-nov
    6 november.

--- app/src/main/java/com/yesitlab/zyvo/activity/guest/RestaurantDetailActivity.kt
+++ app/src/main/java/com/yesitlab/zyvo/activity/guest/RestaurantDetailActivity.kt
@@ -1,15 +1,22 @@
 package com.yesitlab.zyvo.activity.guest
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.Paint
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,6 +28,8 @@ import com.yesitlab.zyvo.R
 import com.yesitlab.zyvo.adapter.AdapterAddOn
 import com.yesitlab.zyvo.adapter.guest.AdapterReview
 import com.yesitlab.zyvo.databinding.ActivityRestaurantDetailBinding
+import java.time.LocalDate
+import java.time.YearMonth
 
 
 class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -31,13 +40,20 @@ class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mapView: MapView
     private var mMap: GoogleMap? = null
 
+    private var currentMonth: YearMonth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        YearMonth.now()
+    } else {
+        TODO("VERSION.SDK_INT < O")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private var selectedDate: LocalDate? = LocalDate.now()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-
         binding = ActivityRestaurantDetailBinding.inflate(LayoutInflater.from(this))
+
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -45,12 +61,11 @@ class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
-
         initialization()
+        updateCalendar()
     }
 
 
@@ -60,8 +75,10 @@ class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.tvReadMoreLess.setCollapsedText("Read More")
         binding.tvReadMoreLess.setCollapsedTextColor(com.yesitlab.zyvo.R.color.green_color_bar)
         binding.recyclerAddOn.adapter = adapterAddon
-        binding.recyclerAddOn.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.recyclerAddOn.layoutManager = GridLayoutManager(this,2)
+        binding.recyclerAddOn.isNestedScrollingEnabled = false
         binding.recyclerReviews.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.recyclerReviews.isNestedScrollingEnabled = false
         binding.recyclerReviews.adapter = adapterReview
         val textView = binding.tvShowMore
         textView.paintFlags = textView.paintFlags or Paint.UNDERLINE_TEXT_FLAG
@@ -79,7 +96,7 @@ class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-     private fun  getAddOnList() :  MutableList<String>{
+    private fun  getAddOnList() :  MutableList<String>{
         var list = mutableListOf<String>()
         list.add("Computer Screen")
         list.add("Bed Sheets")
@@ -89,8 +106,6 @@ class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         list.add("Water Bottle")
         return list
     }
-
-
 
     private fun shareText(text: String) {
         val shareIntent = Intent(Intent.ACTION_SEND)
@@ -129,4 +144,158 @@ class RestaurantDetailActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-}
+    private fun updateCalendar() {
+        // Updates the calendar layout with the current and next month views.
+        val calendarLayout = binding.calendarLayout
+        calendarLayout.removeAllViews()
+        val topMonths = mutableListOf<YearMonth>()
+        val bottomMonths = mutableListOf<YearMonth>()
+
+        // Separate months into top and bottom lists
+        val allMonths = (1..12).map { if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            currentMonth.plusMonths(it.toLong())
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+
+        }
+        allMonths.forEachIndexed { index, month ->
+            if (index % 2 == 0) {
+                topMonths.add(month)
+//            } else {
+//                bottomMonths.add(month)
+            }
+        }
+
+        addMonthView(calendarLayout, currentMonth)
+        // addMonthView(calendarLayout, currentMonth.plusMonths(1))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetTextI18n")
+    private fun addMonthView(parentLayout: LinearLayout, yearMonth: YearMonth) {
+        // Adds a view for the specified month to the parent layout.
+        val monthView = layoutInflater.inflate(R.layout.calendar_month, parentLayout, false)
+
+        val monthTitle = monthView.findViewById<TextView>(R.id.month_title)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            monthTitle.text = "${yearMonth.month.name.lowercase().replaceFirstChar { it.uppercase() }}"
+            //${yearMonth.year}"
+        }
+
+        val daysLayout = monthView.findViewById<LinearLayout>(R.id.days_layout)
+        daysLayout.removeAllViews()
+        listOf("S", "M", "T", "W", "T", "F", "S").forEach { day ->
+            val dayView = layoutInflater.inflate(R.layout.calendar_day_name, daysLayout, false) as TextView
+            dayView.text = day
+            daysLayout.addView(dayView)
+        }
+
+        val weeksLayout = monthView.findViewById<LinearLayout>(R.id.weeks_layout)
+        weeksLayout.removeAllViews()
+        val weeks = generateCalendarWeeks(yearMonth)
+        weeks.forEach { week ->
+            val weekLayout = LinearLayout(this).apply { orientation = LinearLayout.HORIZONTAL }
+            week.forEach { date ->
+                val dateView = layoutInflater.inflate(R.layout.calendar_day, weekLayout, false) as TextView
+                if (date != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        // dateView.text = date.dayOfMonth.toString()
+                        dateView.text = date.dayOfMonth.toString().padStart(2, '0')
+
+                    }
+                    dateView.setOnClickListener {
+                        selectedDate = date
+                        updateCalendar()
+                        // Toast.makeText(requireContext(), "Selected Date: ${date.dayOfMonth} ${date.month.name.lowercase().replaceFirstChar { it.uppercase() }} ${date.year}", Toast.LENGTH_SHORT).show()
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                        when (date) {
+//                            LocalDate.now() -> dateView.setBackgroundResource(R.drawable.current_bg_date)
+//                          //  selectedDate -> dateView.setBackgroundResource(R.drawable.selected_bg)
+//
+//                            else -> dateView.setBackgroundResource(android.R.color.transparent)
+//                        }
+
+                        when (date) {
+                            //LocalDate.now() -> dateView.setBackgroundResource(R.drawable.current_bg_date)
+                            selectedDate -> dateView.setBackgroundResource(R.drawable.current_bg_date)
+                            else -> dateView.setBackgroundResource(R.drawable.date_bg)
+                        }
+                    }
+                    dateView.setTextColor(
+                        if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                date.month == yearMonth.month
+                            } else {
+                                TODO("VERSION.SDK_INT < O")
+                            }
+                        ) Color.BLACK
+                        else Color.GRAY
+                    )
+                }
+                weekLayout.addView(dateView)
+            }
+            weeksLayout.addView(weekLayout)
+        }
+        // Display llPreviousAndNextMonth only for the current month
+        if (yearMonth == currentMonth) {
+            val previous = monthView.findViewById<ImageButton>(R.id.button_previous)
+            previous.setOnClickListener {
+                currentMonth = currentMonth.minusMonths(1)
+                updateCalendar()
+            }
+            val next = monthView.findViewById<ImageButton>(R.id.button_next)
+            next.setOnClickListener {
+                currentMonth = currentMonth.plusMonths(1)
+                updateCalendar()
+            }
+            parentLayout.addView(monthView)
+        } else {
+            // If not the current month, hide the previous and next buttons
+//            val llPreviousAndNextMonth = monthView.findViewById<LinearLayout>(R.id.llPreviousAndNextMonth)
+//            llPreviousAndNextMonth.visibility = View.GONE
+//            parentLayout.addView(monthView)
+        }
+    }
+
+    private fun generateCalendarWeeks(yearMonth: YearMonth): List<List<LocalDate?>> {
+        // Generates a list of weeks, each containing dates for the specified month.
+        val weeks = ArrayList<List<LocalDate?>>()
+        var week = ArrayList<LocalDate?>()
+
+        val firstDayOfMonth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            yearMonth.atDay(1).dayOfWeek.value % 7
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+
+        // Add null values for the days before the start of the month
+        for (i in 0 until firstDayOfMonth) {
+            week.add(null)
+        }
+
+        val daysInMonth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            yearMonth.lengthOfMonth()
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        for (day in 1..daysInMonth) {
+            week.add(yearMonth.atDay(day))
+            if (week.size == 7) {
+                weeks.add(week)
+                week = ArrayList()
+            }
+        }
+
+        // Add null values for the days after the end of the month
+        while (week.size < 7) {
+            week.add(null)
+        }
+        if (week.isNotEmpty()) {
+            weeks.add(week)
+        }
+        return weeks
+    }
+
+
+ }
