42c34c0834bf83b559fbb87894b26aec04e2658d
Nikunj Gupta <64137813+Nikunjgupta-dev@users.noreply.github.com>
2024-11-13 19:27:00 +0530

    nikunj-commit-13-11-24
    Nikunj Commit on 13 november 2024

--- app/src/main/java/com/yesitlab/zyvo/activity/CheckOutPayActivity.kt
+++ app/src/main/java/com/yesitlab/zyvo/activity/CheckOutPayActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yesitlab.zyvo.R
 import com.yesitlab.zyvo.adapter.AdapterAddOn
 import com.yesitlab.zyvo.databinding.ActivityCheckOutPayBinding
@@ -14,6 +15,7 @@ class CheckOutPayActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityCheckOutPayBinding
     lateinit var adapterAddon : AdapterAddOn
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,5 +34,31 @@ class CheckOutPayActivity : AppCompatActivity() {
 
     fun initialization(){
 
+        adapterAddon = AdapterAddOn(this@CheckOutPayActivity,getAddOnList().subList(0,4))
+        binding.recyclerAddOn.layoutManager = LinearLayoutManager(this@CheckOutPayActivity, LinearLayoutManager.VERTICAL ,false)
+        binding.recyclerAddOn.adapter = adapterAddon
+
     }
+
+    private fun getAddOnList(): MutableList<String> {
+
+        var list = mutableListOf<String>()
+
+        list.add("Computer Screen")
+
+        list.add("Bed Sheets")
+
+        list.add("Phone charger")
+
+        list.add("Ring Light")
+
+        list.add("Left Light")
+
+        list.add("Water Bottle")
+
+        return list
+
+    }
+
+
 }
