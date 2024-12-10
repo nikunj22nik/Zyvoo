48785dd2645acc34f8e78428d61585f718a29829
Nikunj Gupta <64137813+Nikunjgupta-dev@users.noreply.github.com>
2024-12-10 16:47:15 +0530

    nikunj commit 10 december 2024
    Nikunj Commit 10 december 2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,13 +28,16 @@ android{
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
         //noinspection DataBindingWithoutKapt
